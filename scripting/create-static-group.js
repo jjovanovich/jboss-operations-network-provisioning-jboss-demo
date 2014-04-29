e70c436a46992f9db8b604c7e2c66cae4e736a39
rbrackma <rbrackma@redhat.com>
2014-04-29 14:29:09 +0200

    adding new changes

---
+++ scripting/create-static-group.js
@@ -0,0 +1,19 @@
+var groupName = args[0];
+var groupType = args[1];
+
+var resourceGroup = findGroup(groupName);
+
+if( resourceGroup != null ) {
+    deleteGroup(resourceGroup.getId());
+}
+
+  if (groupType == "eap6"){
+    var resourceType = getJBossEAP6Type();
+  }
+	else {
+		return resourceType = getLinuxType();
+	}
+
+createGroup(groupName, resourceType);
+
+
