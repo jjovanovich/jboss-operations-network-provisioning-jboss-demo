02a1ca5ad9e8eedece445d0c1a0f3af58dd7905a
rbrackma <rbrackma@redhat.com>
2014-04-29 14:31:02 +0200

    adding new changes

--- scripting/create-static-group.sh
+++ scripting/create-static-group.sh
@@ -1,5 +1,5 @@
 $JON_CLI_HOME/bin/rhq-cli.sh -u rhqadmin -p rhqadmin -s localhost -t 7080 << EOF
 exec -f $JON_SCRIPT_HOME/paas.js
-exec -f $JON_SCRIPT_HOME/create-static-linux-group.js $1 $2
+exec -f $JON_SCRIPT_HOME/create-static-group.js $1 $2
 quit
 EOF
